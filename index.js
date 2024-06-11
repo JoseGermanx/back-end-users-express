9fcebd10debe86e55bf46247540a4698f54309b0
Jose Germán Martínez <jgermanmm@gmail.com>
2024-06-11 12:27:46 -0400

    chore: Add database connection to index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
-
 const app = require('./src/app/app.js');
+const dbConnection = require('./src/database/conexion.js');
+
+
 
 app.listen(3000, () => {
   console.log('Server on port 3000');
 });
+
+dbConnection();
